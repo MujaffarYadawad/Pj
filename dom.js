b306e7ee0654d5f26b1cd243f68ff10ca3081f68
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2022-09-04 11:49:27 +0530

    playing Queryselector and QueryselectorAll

--- dom.js
+++ dom.js
@@ -13,20 +13,47 @@
 //console.log(document.images);
 
 //getelemntsby id
-var headerTitle = document.getElementById('header-title'); 
-var header= document.getElementById('main-header');
+//var headerTitle = document.getElementById('header-title'); 
+//var header= document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent = 'Hello';
 //headerTitle.innerText = 'goodbye';
 //console.log(headerTitle.textContent);
 //headerTitle.innerHTML = '<h3>Helloo</h3>';
 //headerTitle.style.borderBottom = 'Solid 3px #000';
-var li = document.getElementsByTagName('li');
+//var li = document.getElementsByTagName('li');
 //items[1].textContent = 'Hello';
 //items[1].style.fontWeight = 'bold';
 //li[3].style.backgroundColor = 'lightgreen';
 
-for(var i=0;i<li.length;i++){
-  li[i].style.fontWeight = 'bold';
+//for(var i=0;i<li.length;i++){
+  //li[i].style.fontWeight = 'bold';
   
+//}
+
+//quarySelector
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4px #green';
+
+//var input = document.querySelector('input');
+i//nput.value = 'Hello World';
+  
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value="send";
+//var item = document.querySelector('.list-group-item');
+//item.style.color = 'lightgreen';
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color = 'ligthgreen'; 
+
+//querySelectorAll
+var titles =document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';;
+ 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+  odd[i].style.backgroundColor = 'lightgreen';
 }
+
+
+

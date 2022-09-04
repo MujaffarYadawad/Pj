b2f92bb7587f1a464a29670a87c5cb3b77544516
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2022-09-04 09:04:57 +0530

    changes of all bold items

--- dom.js
+++ dom.js
@@ -22,6 +22,10 @@ var header= document.getElementById('main-header');
 //headerTitle.innerHTML = '<h3>Helloo</h3>';
 //headerTitle.style.borderBottom = 'Solid 3px #000';
 var items = document.getElementsByClassName('list-group-item');
-items[1].textContent = 'Hello';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'lightgreen';
+//items[1].textContent = 'Hello';
+//items[1].style.fontWeight = 'bold';
+items[3].style.backgroundColor = 'lightgreen';
+
+for(var i=0;i<items.length;i++){
+  items[i].style.fontWeight = 'bold';
+}
